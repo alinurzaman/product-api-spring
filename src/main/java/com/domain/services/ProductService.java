59665e83cecfee2ded3a2c4b938ecf632d73fad8
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2022-11-21 16:31:21 +0700

    first commit

---
+++ src/main/java/com/domain/services/ProductService.java
@@ -0,0 +1,53 @@
+package com.domain.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.domain.models.entities.Product;
+import com.domain.models.entities.Supplier;
+import com.domain.models.repos.ProductRepo;
+
+@Service
+@Transactional
+public class ProductService {
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product) {
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id) {
+        Optional<Product> temp = productRepo.findById(id);
+        if (!temp.isPresent()) {
+            return null;
+        }
+        return productRepo.findById(id).get();
+    }
+
+    public Iterable<Product> findAll() {
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> findByName(String name) {
+        return productRepo.findByNameContains(name);
+    }
+
+    public void addSupplier(Supplier supplier, Long productId) {
+        Product product = findOne(productId);
+        if (product == null) {
+            throw new RuntimeException("Product with ID: " + productId + " not found.");
+        }
+        product.getSuppliers().add(supplier);
+        save(product);
+    }
+}
